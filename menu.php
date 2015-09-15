24cee5c780f27d60fdd93b924047baac793b4a73
Domenico <domenico.garitta@gmail.com>
2015-09-15 12:10:20 +0200

    VERSIONE: 1.0 INFO:	Integrato il PHP per la creazione del menù e impostato in modo 	dinamico il contenuto dei DIV che, ora, si automodellano per contenere 	tutto ciò che vi sta al suo interno

---
+++ menu.php
@@ -0,0 +1,24 @@
+<ul class="nav site-nav" >
+	<li><a href="#">Home</a><!-- * --></li>
+	<li><a href="#">Profilo</a><!-- * --></li>
+	<li class="flyout">
+		<a href="#">Piano di studi</a>
+		<!-- Flyout -->
+		<ul class="flyout-content nav stacked" id="riquadro">
+			<li class="over"><a href="#">Visualizza piano di studi</a></li>
+			<li  class="over"><a href="#">Gestisci materie integrative</a></li>
+		</ul>
+	</li>
+	<li class="flyout">
+		<a href="#">Documenti</a>
+		<!-- Flyout -->
+		<ul class="flyout-content nav stacked" id="riquadro">
+			<li  class="over"><a href="#">Visualizza documenti</a></li>
+			<li  class="over"><a href="#">Carica documenti</a></li>
+		</ul>
+	</li>
+	<li><a href="#">Contatti</a><!-- * --></li>
+	<li><a href="#">Disconnetti</a><!-- * --></li>
+</ul>
+
+
