80e7435e783c33b791c40f50dc1a2eb2ede965e6
Domenico <domenico.garitta@gmail.com>
2015-09-23 17:53:35 +0200

    VERSIONE: 1.2.3 INFO:	fare in modo di mettere il menù HTML dentro una funzione di php 	in modo tale che, se includo il MENU.PHP ad inizio pagina, non 	stampi il menù nel posto sbagliato ma solo dove richiamo la 	funzione menu(). Questo perchè nel menu.php sono incluse le 	"librerie" utente_loggato.php, la funzione di login e 	dbconnection.php che permettono l'interrogazione del database solo 	al di sotto del loro include_once(). 	Questo per mermettermi di poter usare l'interrogazione al database 	direttamente prima del BODY di ogni pagina.
    DA FARE: provare a creare la funzione php menu() che racchiude il menù
    	evitando di inserire il codice HTML in un echo

--- menu.php
+++ menu.php
@@ -1,15 +1,21 @@
 <?php
 @session_start();
 @include_once 'utente_loggato.php';
+//@include_once 'dbconnection.php';
 
 if(!isset($_SESSION['login'])){
 	@header("location:index.php");
 } else {
 	$utente=$_SESSION['ut'];
 	$utente=unserialize($utente);
-	
+	/*rendere il menù una funzione di PHP in modo tale da poter
+	includere menu.php all'inizio della pagina e poter richiamare il menù 
+	dove si vuole con la funzione menu();
+	Altro motivo per inserire il menù all'interno di una funzione è quello di poter mettere l'include_once in cima 
+	alla pagina in modo tale da far includere nella pagina l'untente loggato, il modulo di login e, quindi, il poter
+	richiamare la connessione al database già ad inizio pagina anzichè a metà pagina*/
 	?>
-
+	
 	<ul class="nav site-nav" >
 		<li><a href="#">Home</a><!-- * --></li>
 		<li><a href="#">Profilo</a><!-- * --></li>
