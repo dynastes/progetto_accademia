7184d369f8a03974d2425763739bd20a09b58d43
Domenico <domenico.garitta@gmail.com>
2015-10-22 18:46:28 +0200

    VERSIONE: 1.4.1 INFO:	Creata una BETA della home degli STUDENTI. Ora possono visualizzare 	gli ultimi avvisi pubblicati dai professori.
    DA FARE: 1) creare la possibilità di visualizzare gli avvisi relativi ai
    	professori dei soli corsi che si seguino
    	2) Aggiungere un controllo all'attuale homepage che esclude gli
    	avvisi che i professori pubblicano agli ADMIN. Quindi mostrare solo
    	quegli avvisi marcati come "PUBBLICO"

--- studenti_home.php
+++ studenti_home.php
@@ -34,10 +34,24 @@
 					<p>Qui verranno visualizzati gli ultimi 10 avvisi pubblicati da docenti o admin. Per vedere lo storico completo degli avvisi, andare nella pagina Avvisi.</p>
 					<div id="elenco-avvisi">
 						<?php
-							$sqlavvisi="SELECT an.Nome, an.Cognome, av.Testo, av.Data_pubblicazione FROM avvisi AS av, anagrafe AS an
-WHERE av.Id_docente=an.Id";
+							$sqlavvisi="SELECT an.Nome, an.Cognome, av.Testo, av.Data_pubblicazione FROM avvisi AS av, anagrafe AS an WHERE av.Id_docente=an.Id";
 							$avvisiLista=$connessione->query($sqlavvisi);
-							
+							echo '<table id="box-caricamenti-principale">';
+							echo '<tr>';
+								echo '<td style="width:17%;" class="box-avvisi-home"><b>Nome</b></td>';
+								echo '<td style="width:17%;" class="box-avvisi-home"><b>Cognome</b></td>';
+								echo '<td style="width:46%;" class="box-avvisi-home"><b>Testo</b></td>';
+								echo '<td style="width:20%;" class="box-avvisi-home"><b>Data Pubblicazione</b></td>';
+							echo '</tr>';
+							while($resAvvisi=$avvisiLista->fetch_assoc()){
+								echo '<tr>';
+									echo '<td style="width:17%;" class="lista-avvisi-home">'.$resAvvisi["Nome"].'</td>';
+									echo '<td style="width:17%;" class="lista-avvisi-home">'.$resAvvisi["Cognome"].'</td>';
+									echo '<td style="width:46%;" class="lista-avvisi-home">'.$resAvvisi["Testo"].'</td>';
+									echo '<td style="width:20%;" class="lista-avvisi-home">'.$resAvvisi["Data_pubblicazione"].'</td>';
+								echo '</tr>';
+							}
+							echo "</table>";
 						?>
 					</div>
 				</div>
