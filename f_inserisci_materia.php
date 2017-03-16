44dc44b513d5fa1d9411b1a105a5c653a04279f1
Domenico <domenico.garitta@gmail.com>
2017-03-16 10:44:48 +0100

    VERSIONE: 1.5.9_7 Aggiustato bug nella pagina f-Inserisci_materia.php

--- f_inserisci_materia.php
+++ f_inserisci_materia.php
@@ -1,15 +1,16 @@
 <?php
-@session_start();
+//@session_start();
 @include_once 'dbconnection.php';
 
 $id_settore=$_POST['settore'];
 $nome_materia=$_POST['nome_materia'];
 
-$_SESSION['id_settore']=$id_settore;
+//$_SESSION['id_settore']=$id_settore;
 
-$sql_inserisci_materia="INSERT INTO materie_anagrafica (Nome_materia, Id_settore) VALUES ('$nome_materia', '$id_settore');";
+$sql_inserisci_materia="INSERT INTO materie_anagrafica (Nome_materia, Id_settore) VALUES (".$nome_materia.", '$id_settore');";
+echo $sql_inserisci_materia;
 $connessione->query($sql_inserisci_materia);
 
-@header('location:admin_inserisci_materia_anagrafe.php');
+//@header('location:admin_inserisci_materia_anagrafe.php');
 
 ?>
