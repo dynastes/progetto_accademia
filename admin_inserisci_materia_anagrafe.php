218653eb9d893897d6bd0aad6384d362077aecb4
JRichard091 <richardigno@hotmail.it>
2017-03-16 11:13:30 +0100

    VERSIONE 1.5.9_8.2

--- admin_inserisci_materia_anagrafe.php
+++ admin_inserisci_materia_anagrafe.php
@@ -52,17 +52,17 @@ Prendere inoltre i seguenti campi dalla tabella "materie_anagrafica":
 
 			<input type="submit" value="Inserisci materia">
 			
-			<label for="nome_materia">Nome materie </label>
-			1 <input type="text" id="nome_materia1" name="nome_materia1"><br>
-			2 <input type="text" id="nome_materia2" name="nome_materia2"><br>
-			3 <input type="text" id="nome_materia3" name="nome_materia3"><br>
-			4 <input type="text" id="nome_materia4" name="nome_materia4"><br>
-			5 <input type="text" id="nome_materia5" name="nome_materia5"><br>
-			6 <input type="text" id="nome_materia6" name="nome_materia6"><br>
-			7 <input type="text" id="nome_materia7" name="nome_materia7"><br>
-			8 <input type="text" id="nome_materia8" name="nome_materia8"><br>
-			9 <input type="text" id="nome_materia9" name="nome_materia9"><br>
-			10 <input type="text" id="nome_materia10" name="nome_materia10"><br>
+			<label for="nome_materia">Nome materie </label><br>
+			1 &nbsp <input type="text" id="nome_materia1" name="nome_materia1"><br>
+			2 &nbsp <input type="text" id="nome_materia2" name="nome_materia2"><br>
+			3 &nbsp <input type="text" id="nome_materia3" name="nome_materia3"><br>
+			4 &nbsp <input type="text" id="nome_materia4" name="nome_materia4"><br>
+			5 &nbsp <input type="text" id="nome_materia5" name="nome_materia5"><br>
+			6 &nbsp <input type="text" id="nome_materia6" name="nome_materia6"><br>
+			7 &nbsp <input type="text" id="nome_materia7" name="nome_materia7"><br>
+			8 &nbsp <input type="text" id="nome_materia8" name="nome_materia8"><br>
+			9 &nbsp <input type="text" id="nome_materia9" name="nome_materia9"><br>
+			10 &nbsp <input type="text" id="nome_materia10" name="nome_materia10"><br>
 		</form>
 	</body>
 </html>
