1d9e1caec7287e4d414ccea8a0b6854d12af6441
JRichard091 <richardigno@hotmail.it>
2017-03-16 11:31:09 +0100

    VERSIONE 1.5.9_8.3 Continua...

--- f_inserisci_materia.php
+++ f_inserisci_materia.php
@@ -15,44 +15,44 @@ $nome_materia9=$_POST['nome_materia9'];
 $nome_materia10=$_POST['nome_materia10'];
 
 //$_SESSION['id_settore']=$id_settore;
-if (isset ($nome_materia1)){
+if (strlen($nome_materia1)>1){
 	$sql_inserisci_materia1="INSERT INTO materie_anagrafica (Nome_materia, Id_settore) VALUES (\"".$nome_materia1."\", ".$id_settore.");";
 	//echo $sql_inserisci_materia;
 	$connessione->query($sql_inserisci_materia1);
 }
-if (isset ($nome_materia2)){
+if (strlen($nome_materia2)>1){
 	$sql_inserisci_materia2="INSERT INTO materie_anagrafica (Nome_materia, Id_settore) VALUES (\"".$nome_materia2."\", ".$id_settore.");";
 	$connessione->query($sql_inserisci_materia2);
 	}
-if (isset ($nome_materia3)){
+if (strlen($nome_materia3)>1){
 	$sql_inserisci_materia3="INSERT INTO materie_anagrafica (Nome_materia, Id_settore) VALUES (\"".$nome_materia3."\", ".$id_settore.");";
 	$connessione->query($sql_inserisci_materia3);
 	}
-if (isset ($nome_materia4)){
+if (strlen($nome_materia4)>1){
 	$sql_inserisci_materia4="INSERT INTO materie_anagrafica (Nome_materia, Id_settore) VALUES (\"".$nome_materia4."\", ".$id_settore.");";
 	$connessione->query($sql_inserisci_materia4);
 	}
-if (isset ($nome_materia5)){
+if (strlen($nome_materia5)>1){
 	$sql_inserisci_materia5="INSERT INTO materie_anagrafica (Nome_materia, Id_settore) VALUES (\"".$nome_materia5."\", ".$id_settore.");";
 	$connessione->query($sql_inserisci_materia5);
 	}
-if (isset ($nome_materia6)){
+if (strlen($nome_materia6)>1){
 	$sql_inserisci_materia6="INSERT INTO materie_anagrafica (Nome_materia, Id_settore) VALUES (\"".$nome_materia6."\", ".$id_settore.");";
 	$connessione->query($sql_inserisci_materia6);
 	}
-if (isset ($nome_materia7)){
+if (strlen($nome_materia7)>1){
 	$sql_inserisci_materia7="INSERT INTO materie_anagrafica (Nome_materia, Id_settore) VALUES (\"".$nome_materia7."\", ".$id_settore.");";
 	$connessione->query($sql_inserisci_materia7);
 	}
-if (isset ($nome_materia8)){
+if (strlen($nome_materia8)>1){
 	$sql_inserisci_materia8="INSERT INTO materie_anagrafica (Nome_materia, Id_settore) VALUES (\"".$nome_materia8."\", ".$id_settore.");";
 	$connessione->query($sql_inserisci_materia8);
 	}
-if (isset ($nome_materia9)){
+if (strlen($nome_materia9)>1){
 	$sql_inserisci_materia9="INSERT INTO materie_anagrafica (Nome_materia, Id_settore) VALUES (\"".$nome_materia9."\", ".$id_settore.");";
 	$connessione->query($sql_inserisci_materia9);
 	}
-if (isset ($nome_materia10)){
+if (strlen($nome_materia10)>1){
 	$sql_inserisci_materia10="INSERT INTO materie_anagrafica (Nome_materia, Id_settore) VALUES (\"".$nome_materia10."\", ".$id_settore.");";
 	$connessione->query($sql_inserisci_materia10);
 	}
