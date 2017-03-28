77b2535fcfc1bec39a478042f849c741be31f9d5
Domenico <domenico.garitta@gmail.com>
2017-03-28 12:53:39 +0200

    VERSIONE: 0.1.3.10 Aggiunto "footer.php" in /shared/

--- admin_inserisci_materia.php
+++ admin_inserisci_materia.php
@@ -107,12 +107,13 @@ if (isset($_POST['avviso']) && $_POST['avviso']!="postato"){
 			</div>
 		</div>
 
+		<?php include_once "shared/footer.php" ?>
 		<!-- INIZIO FOOTER -->
 		<div id="footer" style="bottom:0px;left:0px;width:100%;background-color:black;color:white;height:40px;font-size:14px;float:left">
 				<p align="center">
 				Copyright © 2015 Accademia Di Belle Arti Kandinskij
 				<a href="" rel="nofollow" target="_blank"></a>
 				</p>
-			</div> 
+			</div>
 	</body>
 </html>
---
+++ shared/footer.php
@@ -0,0 +1,7 @@
+<!-- INIZIO FOOTER -->
+<div class="footer" id="footer" style="bottom:0px;left:0px;width:100%;background-color:black;color:white;height:40px;font-size:14px;float:left">
+    <p align="center">
+        Copyright © 2015 Accademia Di Belle Arti Kandinskij
+        <a href="" rel="nofollow" target="_blank"></a>
+    </p>
+</div>
