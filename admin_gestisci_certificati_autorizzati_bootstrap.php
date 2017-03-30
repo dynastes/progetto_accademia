64ac5e2b814309c0a87c5d1e485eb41081b8639b
Deb <Deb>
2017-03-30 12:24:49 +0200

    VERSIONE 0.1.3.20 (GRAFICA) Incluso il footer di daniele che avevo dimenticato di inserire in questa pagina

--- admin_gestisci_certificati_autorizzati_bootstrap.php
+++ admin_gestisci_certificati_autorizzati_bootstrap.php
@@ -68,11 +68,6 @@ if($_SESSION["autorizzato"]===1){
 			</div>
 		</div>
 		<!-- INIZIO FOOTER -->
-		<div id="footer" style="bottom:0px;left:0px;width:100%;background-color:black;color:white;height:40px;font-size:14px;float:left">
-				<p align="center">
-				Copyright © 2015 Accademia Di Belle Arti Kandinskij
-				<a href="" rel="nofollow" target="_blank"></a>
-				</p>
-			</div> 
+		<?php @include_once 'shared/footer.php';?>
 	</body>
 </html>
