6209812cff3e23cda8ad118986d3d2c279de9814
Domenico <domenico.garitta@gmail.com>
2017-04-04 12:24:03 +0200

    VERSIONE: 0.1.3.50 Applicate modifiche per la docenti_visualizza_orario_lezioni_bootstrap.php

--- docenti_visualizza_orario_lezioni_bootstrap.php
+++ docenti_visualizza_orario_lezioni_bootstrap.php
@@ -1,70 +1,19 @@
-
+<?php @include_once 'menu_bootstrap.php'; ?>
 <html>
 	<head>
 		<?php @include_once 'shared/head_inclusions.php';?>
-		<!-- INCLUSIONEPRE FULLCALENDAR -->
-		<link href='fullcalendar/css/fullcalendar.css' rel='stylesheet' />
-		<link href='fullcalendar/css/fullcalendar.print.css' rel='stylesheet' media='print' />
-		<script src='fullcalendar/js/moment.min.js'></script>
-		<script src='fullcalendar/js/jquery.min.js'></script>
-		<script src='fullcalendar/js/fullcalendar.min.js'></script>
-		<script src= 'fullcalendar/js/fullcalendar.js'></script>
-		<script>
-			$(document).ready(function() {
-				//creazione della stringa giorno
-				var data = new Date();
-				var giorno=data.getDate();
-				var mese=data.getMonth()+1;
-				var anno=data.getFullYear();
-				var oggi;
-				oggi=anno+'-'+mese+'-'+giorno;
+		<?php @include_once 'shared/head_fullcalendar_inclusions.php';?>
 
-				var calendar = $('#calendar').fullCalendar({
-				header: {
-						left: 'prev,next today',
-						center: 'title',
-						right: 'month,agendaWeek,agendaDay'
-					},
-					defaultDate: oggi,
-					editable: false,
-					
-					events: "http://localhost/Accademia/fullcalendar/events.php",
-					selectable: false,
-					selectHelper: false,
-					editable: false
-					
-					
-				});
-				
-			});
-
-		</script>
-		<style>
-
-			body {
-				margin: 40px 10px;
-				padding: 0;
-				font-family: "Lucida Grande",Helvetica,Arial,Verdana,sans-serif;
-				font-size: 14px;
-			}
-
-			#calendar {
-				max-width: 900px;
-				margin: 0 auto;
-			}
-
-		</style>
 	</head>
 	<body>
-	<div class="container">
-		<?php @include_once 'menu_bootstrap.php'; ?>
-	<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-   <?php menu(); ?>
-	</div>			
-			<div id="calendar"  style="margin-top:10px; margin-bottom:50px;"></div>
+		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			<?php menu(); ?>
 		</div>
-		
-<?php @include_once 'shared/footer.php'; ?>
+		<div class="container">
+			<div id="calendar"></div>
+		</div>
+
+		<?php @include_once 'shared/footer.php'; ?>
 
 	</body>
 </html>
