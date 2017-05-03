e18a310f9629774e4837f43615cc2dc7a1f2d8e4
Domenico <domenico.garitta@gmail.com>
2017-05-03 13:01:03 +0200

    Modifiche:    admin_inserisci_studenti.php: ora è in bootstrap. Non so se le query funzionano o meno

--- admin_inserisci_studenti.php
+++ admin_inserisci_studenti.php
@@ -29,61 +29,32 @@ if (isset($_POST['avviso']) && $_POST['avviso']!="postato"){
 			</div>
 
 			<form method="post" action="admin_inserisci_studenti_query.php">
-				<div class="row">
-					<
-				</div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-				<div class="col-md-4">
-					<div class="row form-group">
+				<div class="row form-group">
+					<div class="col-md-4">
 						<label for="usermail">Nome:</label>
 						<input class="form-control"  type="text" name="nome-studente" required/>
 					</div>
 
-					<div class="row form-group">
-						<label for="usermail">Diploma:&nbsp;</label>
-						<input class="form-control"  type="text" name="diploma-studente" required/>
-					</div>
-
-					<div class="row form-group">
-						<label for="usermail">Codice Fiscale:</label>
-						<input class="form-control"  type="text" name="codice-fiscale" required/>
-					</div>
-
-					<div class="row form-group">
-						<label for="usermail">Indirizzo:</label>
-						<input class="form-control"  type="text" name="indirizzo" required/>
-					</div>
-				</div> <!-- /col-md-4 (1) -->
-
-				<div class="col-md-4">
-
-					<div class="row form-group">
+					<div class="col-md-4">
 						<label for="usermail">Cognome: </label>
 						<input class="form-control"  type="text" name="cognome-studente" required/>
 					</div>
 
-					<div class="row form-group">
-						<label for="usermail">Corso:&nbsp;</label>
-						<select name="corso-studente">
+					<div class="col-md-4">
+						<label for="usermail">Matricola:</label>
+						<input class="form-control"  type="text" name="matricola-studente" required/>
+					</div>
+				</div> <!-- /row form-group (1) -->
+
+				<div class="row form-group">
+					<div class="col-md-4">
+						<label for="usermail">Diploma:</label>
+						<input class="form-control"  type="text" name="diploma-studente" required/>
+					</div>
+
+					<div class="col-md-4">
+						<label for="usermail">Corso:</label>
+						<select class="form-control" name="corso-studente">
 							<?php
 							$sqlCorso="SELECT Nome_corso, Id FROM corsi";
 							$resCorsi=$connessione->query($sqlCorso);
@@ -94,66 +65,86 @@ if (isset($_POST['avviso']) && $_POST['avviso']!="postato"){
 						</select>
 					</div>
 
-					<div class="row form-group">
-						<label for="usermail">Email:</label>
-						<input class="form-control"  type="text" name="email" required/>
-					</div>
-
-					<div class="row form-group">
-						<label for="usermail">Telefono: </label>
-					<input class="form-control"  type="text" name="telefono" required/ />
-					</div>
-
-				</div> <!-- /col-md-4 (2) -->
-
-				<div class="col-md-4">
-					<div class="row form-group">
-						<label for="usermail">Matricola:&nbsp;</label>
-						<input class="form-control"  type="text" name="matricola-studente" required/>
-					</div>
-
-					<div class="row form-group">
-						<label for="usermail">Data Nascita:&nbsp;</label>
-						<select name="giorno-nascita">
-							<?php
-							for ($i=1; $i < 32; $i++) {
-								echo '<option value="'.$i.'">'.$i.'</option>';
-							 }
-							?>
-						</select>
-						<select name="mese-nascita">
-							<option value="01">Gennaio</option>
-							<option value="02">Febbraio</option>
-							<option value="03">Marzo</option>
-							<option value="04">Aprile</option>
-							<option value="05">Maggio</option>
-							<option value="06">Giugno</option>
-							<option value="07">Luglio</option>
-							<option value="08">Agosto</option>
-							<option value="09">Settembre</option>
-							<option value="10">Ottobre</option>
-							<option value="11">Novembre</option>
-							<option value="12">Dicembre</option>
-						</select>
-						<select name="anno-nascita">
-							<?php
-								$anno=date("Y");
-								for ($i=1950; $i < $anno; $i++) {
+					<div class="col-md-4">
+						<label for="usermail">Data Nascita:</label>
+						<div class="form-inline">
+							<select class="form-control"  name="giorno-nascita">
+								<?php
+								for ($i=1; $i < 32; $i++) {
 									echo '<option value="'.$i.'">'.$i.'</option>';
 								 }
-							?>
-						</select>
+								?>
+							</select>
+							<select class="form-control" name="mese-nascita">
+								<option value="01">Gennaio</option>
+								<option value="02">Febbraio</option>
+								<option value="03">Marzo</option>
+								<option value="04">Aprile</option>
+								<option value="05">Maggio</option>
+								<option value="06">Giugno</option>
+								<option value="07">Luglio</option>
+								<option value="08">Agosto</option>
+								<option value="09">Settembre</option>
+								<option value="10">Ottobre</option>
+								<option value="11">Novembre</option>
+								<option value="12">Dicembre</option>
+							</select>
+							<select class="form-control" name="anno-nascita">
+								<?php
+									$anno=date("Y");
+									for ($i=1950; $i < $anno; $i++) {
+										echo '<option value="'.$i.'">'.$i.'</option>';
+									 }
+								?>
+							</select>
+						</div>
+					</div>
+				</div> <!-- /row form-group (2) -->
+
+				<div class="row form-group">
+					<div class="col-md-4">
+						<label for="usermail">Codice Fiscale:</label>
+						<input class="form-control"  type="text" name="codice-fiscale" required/>
 					</div>
 
-					<div class="row form-group">
+					<div class="col-md-4">
+						<label for="usermail">Email:</label>
+						<div class="input-group">
+							<div class="input-group-addon">@</div>
+							<input class="form-control"  type="text" name="email" required/>
+						</div>
+
+					</div>
+
+					<div class="col-md-4">
 						<label for="usermail">Residenza:</label>
 						<input class="form-control"  type="text" name="residenza" required/>
 					</div>
-
-				</div> <!-- /col-md-4 (3) -->
+				</div> <!-- /row form-group (3) -->
 
 				<div class="row form-group">
-					<input class="btn btn-info"   type="submit" value="Inserisci lo studente">
+					<div class="col-md-4">
+						<label for="usermail">Indirizzo:</label>
+						<input class="form-control"  type="text" name="indirizzo" required/>
+					</div>
+
+					<div class="col-md-4">
+						<label for="usermail">Telefono: </label>
+						<div class="input-group">
+							<div class="input-group-addon"><span class="glyphicon glyphicon-earphone"></span></div>
+							<input class="form-control"  type="text" name="telefono" required/ />
+						</div>
+					</div>
+
+					<div class="col-md-4">
+
+					</div>
+				</div> <!-- /row form-group (4) -->
+				<div class="row form-group pull-right">
+					<div class="col-md-12">
+						<input class="btn btn-info"   type="submit" value="Inserisci lo studente">
+					</div>
+
 				</div>
 			</form>
 
