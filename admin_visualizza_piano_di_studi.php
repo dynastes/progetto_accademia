59dbffcb4d71292f3a026e59bd9c8304cab5e21d
Domenico <domenico.garitta@gmail.com>
2017-05-24 12:28:03 +0200

    Dinamicizzata la pagina seguente: 	modified:   admin_visualizza_piano_di_studi.php
    Ora è possibile ottenere una lista dinamica di tutti i corsi per categoria
    
    COSA NON FUNZIONA:
    - La query non visualizza i corsi che fanno parte di quella specifica offerta formativa, ma TUTTE quelle che fanno parte di quel dipartimento

--- admin_visualizza_piano_di_studi.php
+++ admin_visualizza_piano_di_studi.php
@@ -21,6 +21,27 @@ Prendere inoltre i seguenti campi dalla tabella "materie_anagrafica":
 */
 /*
 <meta charset="UTF-8">	*/
+
+//prendo la lista dell'offerta formativa (l'ID e stringhe) ...
+/*$sql_offerta_formativa="SELECT Nome FROM offerta_formativa GROUP BY Nome";
+$query_offerta_formativa=$connessione->query($sql_offerta_formativa);*/
+//$res_offerta_formativa=$query_offerta_formativa->fetch_assoc();
+
+//...prendo la lista di dipartimenti per ogni Offerta formativa esistente
+$sql_dipartimenti_triennio="SELECT Id_dipartimento FROM offerta_formativa WHERE Nome='Triennio'";
+$query_dipartimenti_triennio=$connessione->query($sql_dipartimenti_triennio);
+
+$sql_dipartimenti_biennio="SELECT Id_dipartimento FROM offerta_formativa WHERE Nome='Biennio'";
+$query_dipartimenti_biennio=$connessione->query($sql_dipartimenti_biennio);
+
+
+$sql_dipartimenti_ciclo_unico="SELECT Id_dipartimento FROM offerta_formativa WHERE Nome='Ciclo_unico'";
+$query_dipartimenti_ciclo_unico=$connessione->query($sql_dipartimenti_ciclo_unico);
+
+
+
+
+
 ?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="it" lang="it">
@@ -44,33 +65,43 @@ Prendere inoltre i seguenti campi dalla tabella "materie_anagrafica":
 				<div class="tab-pane active" id="triennio">
 					<div class="row">
 						<div class="form-group">
+<?php
+	//echo $query_dipartimenti_triennio;
+	while ($res_dipartimenti_triennio=$query_dipartimenti_triennio->fetch_assoc()) {
+		//prendo il nome del dipartimento in cui siamo ora
+		$sql_dipartimento_nome="SELECT Id, Nome_dipartimento FROM dipartimenti WHERE Id=".$res_dipartimenti_triennio["Id_dipartimento"]." LIMIT 1";
+		$query_dipartimento_nome=$connessione->query($sql_dipartimento_nome);
+		$res_dipartimento_nome=$query_dipartimento_nome->fetch_assoc();
+		?>
+							<h4><?php echo "Dipartimento di ".$res_dipartimento_nome["Nome_dipartimento"]; ?></h4>
+							<ul>
+		<?php
+		//qui stampo tutti i corsi che fanno parte: di questo dipartimento;
+		$sql_corsi="SELECT Id, Nome_corso FROM corsi WHERE Id_dipartimento=".$res_dipartimento_nome["Id"];
+		$query_corsi=$connessione->query($sql_corsi);
+		while ($res_corsi=$query_corsi->fetch_assoc()) {
+			?>
+								<li>
+			<?php echo "
+									<a href='admin_modifica_piano_di_studi.php?corso=&offerta=1'>".
+										$res_corsi['Nome_corso']."
+									</a>";
+			?>
+								</li>
+			<?php
+		}
+		?>
+								<li><a href="admin_modifica_piano_di_studi.php?corso=1&offerta=1">Pittura</a></li>
+							</ul>
+		<?php
+	}
+?>
 							<h4>Dipartimento di Arti Visive</h4>
 							<ul>
 								<li><a href="admin_modifica_piano_di_studi.php?corso=1&offerta=1">Pittura</a></li>
 								<li><a href="admin_modifica_piano_di_studi.php?corso=2&offerta=1">Scultura</a></li>
 								<li><a href="admin_modifica_piano_di_studi.php?corso=3&offerta=1">Decorazione</a></li>
 								<li><a href="admin_modifica_piano_di_studi.php?corso=4&offerta=1">Grafica</a></li>
-							</ul>
-							<h4>Dipartimento di Progettazione e Arti Applicate</h4>
-							<ul>
-								<li><a href="admin_modifica_piano_di_studi.php?corso=5&offerta=1">Scenografia</a></li>
-								<li><a href="admin_modifica_piano_di_studi.php?corso=6&offerta=1">Fashion Design</a></li>
-								<li><a href="admin_modifica_piano_di_studi.php?corso=7&offerta=1">Graphic Design</a></li>
-								<li><a href="admin_modifica_piano_di_studi.php?corso=8&offerta=1">Nuove Tecnologie dell’Arte</a></li>
-								<ul>
-									<li><a href="admin_modifica_piano_di_studi.php">Indirizzo Foto, Cinema e Televisione</a></li>
-								</ul>
-							</ul>
-							<h4>Dipartimento di Comunicazione e Didattica dell’Arte</h4>
-							<ul>
-								<li><a href="admin_modifica_piano_di_studi.php?corso=9&offerta=1">Valorizzazione Dei Beni Culturali</a></li>
-								<ul>
-									<li><a href="admin_modifica_piano_di_studi.php">Indirizzo Archeologia</a></li>
-								</ul>
-								<li><a href="admin_modifica_piano_di_studi.php?corso=10&offerta=1">Comunicazione e Didattica dell’Arte</a></li>
-								<ul>
-									<li><a href="admin_modifica_piano_di_studi.php">Indirizzo Arte Terapia</a></li>
-								</ul>
 							</ul>
 						</div>
 					</div>
@@ -102,7 +133,7 @@ Prendere inoltre i seguenti campi dalla tabella "materie_anagrafica":
 								</ul>
 								<li><a href="admin_modifica_piano_di_studi.php?corso=17&offerta=2">Didattica Multimediale</a></li>
 							</ul>
-						
+
 						</div>
 					</div>
 				</div>
