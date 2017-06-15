83a6ade79ced8a1c85f8c89de2f613c5d5e38838
DanieleSupinfo <cozzolino_daniele@virgilio.it>
2017-06-15 13:55:56 +0200

    versione 0.1.9 profilo e modifica profilo aggiunta

---
+++ profilo_bootstrap.php
@@ -0,0 +1,52 @@
+<?php
+
+
+@include_once 'shared/head_inclusions.php';
+@include_once 'shared/menu_bootstrap.php';
+@include_once 'dbconnection.php';
+
+$A= $_SESSION['login'];
+$sql = "SELECT * from anagrafe where ID='$A'";
+
+$result = $connessione->query($sql);
+while($row = $result->fetch_assoc()) {
+	$Id=$row['Id'];
+	$Nome=$row['Nome'];
+	$Cognome=$row['Cognome'];
+	$Username=$row['Username'];
+	$Password=$row['Password'];
+}
+
+
+?>
+
+ <!DOCTYPE html>
+
+<html>
+<head>
+	<?php @include_once 'shared/head_inclusions.php';?> 
+</head>
+<body>
+
+	
+   <?php menu(); ?>
+		
+  <div class="container">
+<form method="post" action="modifica_profilo.php">
+
+In questa pagina puoi modificare le tue credenziali di accesso.<br>
+Le tue attuali credenziali sono:</br>
+
+<p>Nome: <input type="text" name="Nome" value="<?php echo $Nome;?>" /></p> 
+<p>Cognome: <input type="text" name="Cognome" value="<?php echo $Cognome;?>" /></p> 
+<p>Username: <input type="text" name="Username" value="<?php echo $Username;?> " /></p>  
+<p>Password: <input type="text" name="Password" value="<?php echo $Password;?> " /></p> 
+<input type="submit" value="Invia richiesta modifica profilo">
+							</div>
+							
+</form>
+
+<?php @include_once 'shared/footer.php'; ?>
+
+</body>
+</html>
