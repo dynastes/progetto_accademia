fcfe39cb39d9e64aba65d41deafb6ef2646dbc6c
JRichard091 <richardignohotmail.it>
2017-06-19 18:58:42 +0200

    modified:   admin_modifica_piano_di_studi.php 	modified:   admin_visualizza_materie_anagrafe.php 	modified:   css/over-bootstrap.css 	modified:   shared/menu.php

--- admin_modifica_piano_di_studi.php
+++ admin_modifica_piano_di_studi.php
@@ -55,23 +55,30 @@ $sql_id_offerta_formativa="SELECT Id FROM offerta_formativa WHERE Id_dipartiment
 				<form action="admin_crea_piano_di_studi_query.php" method="post">
 					<div class="row">
 						<div class="form-group">
-							<table class="table">
-								<tr><th colspan="8" style="background-color:#c00000; color:white; text-align:center">DIPARTIMENTO DI <?php echo strtoupper($nome_dipartimento); ?></th></tr>
-								<tr><th colspan="8" style="text-align:center">CORSO<?php echo $testo_corso; ?></th></tr>
-								<tr><th colspan="8" style="background-color:#c00000; color:white; text-align:center"><?php echo strtoupper($nome_corso); ?></th></tr>
-								<tr><th colspan="8" style="text-align:center">Piano di Studi Consigliato</th></tr>
-								<tr style="background-color:#c00000; color:white;"><th>Codice</th><th>Settore</th><th>Campo Disciplinare</th><th style="text-align:center">Ore</th><th style="text-align:center">CFA</th>
+							<table class="table table-curved">
+							<!-- <table class="table">							 -->
+								<tr><th class="title" colspan="9" style="background-color:#c00000; color:white; text-align:center">DIPARTIMENTO DI <?php echo strtoupper($nome_dipartimento); ?></th></tr>
+								<tr><th class="title" colspan="9" style="text-align:center">CORSO<?php echo $testo_corso; ?></th></tr>
+								<tr><th class="title" colspan="9" style="background-color:#c00000; color:white; text-align:center"><?php echo strtoupper($nome_corso); ?></th></tr>
+								<tr><th class="title" colspan="9" style="text-align:center">Piano di Studi Consigliato</th></tr>
+								<tr style="background-color:#c00000; color:white;"><th></th><th style="text-align:center;">Codice</th><th>Settore</th><th>Campo Disciplinare</th><th style="text-align:center">Ore</th><th style="text-align:center">CFA</th>
 								<th style="text-align:center">Tipo</th><th></th><th></th></tr>
 <?php
 $crediti_totali=0;
 for ($i = 1; $i <= $anni; ++$i) {
+	$Conta_attivita_base=0;
+	$Conta_attivita_caratterizzante=0;
+	$Conta_attivita_integrative=0;
+	$Conta_attivita_obbligatorie=0;
 	$crediti_anno=0; 
 ?>
-								<tr><th colspan="8" style="background-color:#00b0f0; color:white; text-align:center;">ANNO <?php echo $i;?></th></tr>
-								<tr><th colspan="8" style="background-color:#0070c0; text-align:center;">Attività Formative di Base</th></tr>
+								<tr><th colspan="9" style="background-color:#00b0f0; color:white; text-align:center;">ANNO <?php echo $i;?></th></tr>
+								<!-- <tr><th colspan="8" style="background-color:#0070c0; text-align:center;">Attività Formative di Base</th></tr> -->
 <?php
  $sql_carica_piani="SELECT * FROM materie_piano WHERE Anno=".$i." AND Categoria='Base' AND Id_corso=".$id_corso;
  $res_piani=$connessione->query($sql_carica_piani);
+$rowspan_base=mysqli_num_rows($res_piani);
+//echo "num:".$rowspan_base;
  while($res=$res_piani->fetch_assoc()) {
 	//$Id_corso=$res['Id_corso'];
 	$id_materia=$res['Id_materia'];
@@ -95,16 +102,23 @@ for ($i = 1; $i <= $anni; ++$i) {
 			$nome_settore=$res['Settore'];
 ?>
 								<tr>
-									<td> <?php echo $codice_settore; ?></td><td><?php echo $nome_settore; ?> </td>
+								<?php 
+								if($Conta_attivita_base==0){echo "<th rowspan=".$rowspan_base." style=\"background-color:#93C9FF; vertical-align:middle; text-align:center;\">Attività Formative di Base</th>"; $Conta_attivita_base=1;}
+								//if($Conta_attivita_base==0){echo "<th rowspan=".$rowspan_base." style=\"vertical-align:middle; text-align:center;\">Attività Formative di Base</th>"; $Conta_attivita_base=1;}
+								?>
+									
+									<td style="text-align:center;"><?php echo $codice_settore; ?></td><td><?php echo $nome_settore; ?> </td>
 									<td><?php echo $nome_materia." ".$modulo; ?></td><td style="text-align:center"><?php echo $ore; ?></td>
 									<td style="text-align:center"><?php echo $cfa; ?></td><td style="text-align:center"><?php echo $tipo;?></td>
 									<td><a>Modifica</a></td>
 									<td><a>Elimina<?php }}}?></a></td>
 								</tr>
-								<tr><th colspan="8" style="background-color:#ffff00;text-align:center;">Attività Formative Caratterizzanti</th></tr>
+								<!-- <tr><th colspan="8" style="background-color:#ffff00;text-align:center;">Attività Formative Caratterizzanti</th></tr> -->
 <?php
- $sql_carica_piani="SELECT * FROM materie_piano WHERE Anno=".$i." AND Categoria='Caratterizzante' AND Id_corso=".$id_corso;
- $res_piani=$connessione->query($sql_carica_piani);
+$sql_carica_piani="SELECT * FROM materie_piano WHERE Anno=".$i." AND Categoria='Caratterizzante' AND Id_corso=".$id_corso;
+$res_piani=$connessione->query($sql_carica_piani);
+$rowspan_caratterizzante=mysqli_num_rows($res_piani);
+//echo "num:".$rowspan_caratterizzante;
  while($res=$res_piani->fetch_assoc()) {
 	
 	$id_materia=$res['Id_materia'];
@@ -128,16 +142,22 @@ for ($i = 1; $i <= $anni; ++$i) {
 			$nome_settore=$res['Settore'];
 ?>
 								<tr>
-									<td> <?php echo $codice_settore; ?></td><td><?php echo $nome_settore; ?> </td>
+<?php 
+if($Conta_attivita_caratterizzante==0){echo "<th rowspan=".$rowspan_caratterizzante." style=\"background-color:#FFFF78; vertical-align:middle; text-align:center;\">Attività Formative Caratterizzanti</th>"; $Conta_attivita_caratterizzante=1;}
+//if($Conta_attivita_caratterizzante==0){echo "<th rowspan=".$rowspan_caratterizzante." style=\"vertical-align:middle; text-align:center;\">Attività Formative Caratterizzanti</th>"; $Conta_attivita_caratterizzante=1;}
+?>
+									<td style="text-align:center;"><?php echo $codice_settore; ?></td><td><?php echo $nome_settore; ?> </td>
 									<td><?php echo $nome_materia." ".$modulo; ?></td><td style="text-align:center"><?php echo $ore; ?></td>
 									<td style="text-align:center"><?php echo $cfa; ?></td><td style="text-align:center"><?php echo $tipo;?></td>
 									<td><a>Modifica</a></td>
 									<td><a>Elimina<?php }}}?></a></td>
 								</tr>
-								<tr><th colspan="8" style="background-color:#ff99ff;text-align:center;">Attività Formative Integrative o Affini</th></tr>
+								<!-- <tr><th colspan="8" style="background-color:#ff99ff;text-align:center;">Attività Formative Integrative o Affini</th></tr> -->
 <?php
- $sql_carica_piani="SELECT * FROM materie_piano WHERE Anno=".$i." AND Categoria='Integrativa' AND Id_corso=".$id_corso;
- $res_piani=$connessione->query($sql_carica_piani);
+$sql_carica_piani="SELECT * FROM materie_piano WHERE Anno=".$i." AND Categoria='Integrativa' AND Id_corso=".$id_corso;
+$res_piani=$connessione->query($sql_carica_piani);
+$rowspan_integrativa=mysqli_num_rows($res_piani);
+//echo "num:".$rowspan_integrativa;
  while($res=$res_piani->fetch_assoc()) {
 	
 	$id_materia=$res['Id_materia'];
@@ -161,7 +181,11 @@ for ($i = 1; $i <= $anni; ++$i) {
 			$nome_settore=$res['Settore'];
 ?>
 								<tr>
-									<td> <?php echo $codice_settore; ?></td><td><?php echo $nome_settore; ?> </td>
+<?php 
+if($Conta_attivita_integrative==0){echo "<th rowspan=".$rowspan_integrativa." style=\"background-color:#ff99ff; vertical-align:middle; text-align:center;\">Attività Formative Caratterizzanti</th>"; $Conta_attivita_integrative=1;}
+//if($Conta_attivita_integrative==0){echo "<th rowspan=".$rowspan_integrativa." style=\"vertical-align:middle; text-align:center;\">Attività Formative Caratterizzanti</th>"; $Conta_attivita_integrative=1;}
+?>
+									<td style="text-align:center;"><?php echo $codice_settore; ?></td><td><?php echo $nome_settore; ?> </td>
 									<td><?php echo $nome_materia." ".$modulo; ?></td><td style="text-align:center"><?php echo $ore; ?></td>
 									<td style="text-align:center"><?php echo $cfa; ?></td><td style="text-align:center"><?php echo $tipo;?></td>
 									<td><a>Modifica</a></td>
@@ -171,9 +195,11 @@ for ($i = 1; $i <= $anni; ++$i) {
 <?php
 $sql_esistenza_obbligatorie="SELECT * FROM materie_piano WHERE Anno=".$i." AND Categoria='Obbligatoria' AND Id_corso=".$id_corso;
 $res_obbligatorie=$connessione->query($sql_esistenza_obbligatorie);
+$rowspan_obbligatorie=mysqli_num_rows($res_obbligatorie);
+//echo "row span obb: ".$rowspan_obbligatorie;
 $res=$res_obbligatorie->fetch_assoc();
 if($res!=false){
-	echo "<tr><th colspan=\"8\" style=\"background-color:orange;text-align:center;\">Attività Formative Obbligatorie</th></tr>";
+	//echo "<tr><th colspan=\"8\" style=\"background-color:orange;text-align:center;\">Attività Formative Obbligatorie</th></tr>";
 	$sql_carica_piani="SELECT * FROM materie_piano WHERE Anno=".$i." AND Categoria='Obbligatoria' AND Id_corso=".$id_corso;
 	$res_piani=$connessione->query($sql_carica_piani);
 	while($res=$res_piani->fetch_assoc()) {
@@ -199,7 +225,12 @@ if($res!=false){
 				$nome_settore=$res['Settore'];
 								//Parte della tabella che riguarda le "Attività Formative Obbligatorie"
 								echo "<tr>";
-									echo "<td>".$codice_settore."</td><td>".$nome_settore."</td>";
+									
+									
+if($Conta_attivita_obbligatorie==0){echo "<th rowspan=".$rowspan_obbligatorie." style=\"background-color:#FFB464; vertical-align:middle; text-align:center;\">Attività Formative Obbligatorie</th>"; $Conta_attivita_obbligatorie=1;}
+//if($Conta_attivita_obbligatorie==0){echo "<th rowspan=".$rowspan_obbligatorie." style=\"vertical-align:middle; text-align:center;\">Attività Formative Obbligatorie</th>"; $Conta_attivita_obbligatorie=1;}
+
+									echo "<td style=\"text-align:center;\">".$codice_settore."</td><td>".$nome_settore."</td>";
 									echo "<td>".$nome_materia." ".$modulo."</td><td style=\"text-align:center\">".$ore."</td>";
 									echo "<td style=\"text-align:center\">".$cfa."</td><td style=\"text-align:center\">".$tipo."</td>";
 									echo "<td><a>Modifica</a></td>";
@@ -211,9 +242,10 @@ if($res!=false){
 }
 ?>
 								<tr>
-									<td></td><td></td><td style="background-color:#00ff00; text-align:right">Crediti</td>
+									<td></td>
+									<td></td><td></td><th style="background-color:#00ff00; text-align:right">Crediti</th>
 									<td style="background-color:#00ff00;"></td>
-									<td style="background-color:#00ff00; text-align:center"><?php $crediti_totali+=$crediti_anno; echo $crediti_anno; ?></td>
+									<th style="background-color:#00ff00; text-align:center"><?php $crediti_totali+=$crediti_anno; echo $crediti_anno; ?></th>
 									<td colspan="3" style="background-color:#00ff00;"></td>
 								</tr>
 								
@@ -221,9 +253,10 @@ if($res!=false){
 }//Chiusura ciclo for "anni"
 ?>
 								<tr>
-									<td colspan="3" style="background-color:#00ff00; text-align:right">Totale Crediti Formativi previsti nel Triennio </td>
+									<td></td>
+									<th colspan="3" style="background-color:#00ff00; text-align:right">Totale Crediti Formativi previsti nel Triennio </th>
 									<td style="background-color:#00ff00;"></td>
-									<td style="background-color:#00ff00; text-align:center"><?php echo $crediti_totali; ?></td>
+									<th style="background-color:#00ff00; text-align:center"><?php echo $crediti_totali; ?></th>
 									<td colspan="3" style="background-color:#00ff00; text-align:center"></td>
 								</tr>
 							</table>
--- admin_visualizza_materie_anagrafe.php
+++ admin_visualizza_materie_anagrafe.php
@@ -10,51 +10,47 @@
 		<?php @include_once 'shared/menu.php';?> 
 		<script src="sorttable.js"></script>
 	</head>
-	
+
 	<body>
-	<?php menu(); ?>
+		<?php menu(); ?>
 		<div class="container">
 			<div class="page-header">
 				<h1>Modifica anagrafica materie</h1>
 			</div>
-			<div class="container">
-				
-					<div class="row">
-						<div class="form-group">
-		<table class="sortable" class="table">
-			<tr>
-				<!-- <th style="text-align:center">ID</th> -->
-				<th style="text-align:center" class="sorttable_nosort">Codice Settore</th>
-				<th style="text-align:center">Settore</th>
-				<th style="text-align:center">Nome</th>
-				<th class="sorttable_nosort" style="text-align:center"><!-- Modifica --></th>
-				<th class="sorttable_nosort" style="text-align:center"><!-- Elimina --></th>
-			</tr>
-			
-<?php
-$sql_carica_materie="SELECT * FROM materie_anagrafica";
-//echo "Query: ".$sql_carica_materie;
-$res_materie=$connessione->query($sql_carica_materie);
-while($res=$res_materie->fetch_assoc()) {
-	$sql_carica_settore="SELECT * FROM settore WHERE ID=".$res['Id_settore'];
-	//echo "Query: ".$sql_carica_settore;
-$res_settore=$connessione->query($sql_carica_settore);
-while($res2=$res_settore->fetch_assoc()) {
-  
-?>
-			<tr>
-				<!-- <td style="text-align:center"><?php //echo $res['Id']; ?></td> -->
-				<td style="text-align:center"><?php echo $res2['Codice']; ?></td>
-				<td><?php echo $res2['Settore']; ?></td>
-				<td><?php echo $res['Nome_materia']; ?></td>
-				<td><a href="admin_modifica_materia_anagrafe.php?ID=<?php echo $res['Id']; ?>">Modifica</a></td>
-				<td><a href="admin_elimina_materia_anagrafe_query.php?ID=<?php echo $res['Id']; ?>">Elimina<?php }}?></a></td>
-			</tr>
-		</table>
+			<div class="row">
+				<div class="form-group">
+					<table class="table sortable">
+						<tr>
+							<!-- <th style="text-align:center">ID</th> -->
+							<th style="text-align:center" class="sorttable_nosort">Codice Settore</th>
+							<th style="text-align:center">Settore</th>
+							<th style="text-align:center">Nome</th>
+							<th class="sorttable_nosort" style="text-align:center"><!-- Modifica --></th>
+							<th class="sorttable_nosort" style="text-align:center"><!-- Elimina --></th>
+						</tr>
+
+						<?php
+						$sql_carica_materie="SELECT * FROM materie_anagrafica";
+						//echo "Query: ".$sql_carica_materie;
+						$res_materie=$connessione->query($sql_carica_materie);
+						while($res=$res_materie->fetch_assoc()) {
+						$sql_carica_settore="SELECT * FROM settore WHERE ID=".$res['Id_settore'];
+						//echo "Query: ".$sql_carica_settore;
+						$res_settore=$connessione->query($sql_carica_settore);
+						while($res2=$res_settore->fetch_assoc()) {
+
+						?>
+						<tr>
+							<!-- <td style="text-align:center"><?php //echo $res['Id']; ?></td> -->
+							<td style="text-align:center"><?php echo $res2['Codice']; ?></td>
+							<td><?php echo $res2['Settore']; ?></td>
+							<td><?php echo $res['Nome_materia']; ?></td>
+							<td><a href="admin_modifica_materia_anagrafe.php?ID=<?php echo $res['Id']; ?>">Modifica</a></td>
+							<td><a href="admin_elimina_materia_anagrafe_query.php?ID=<?php echo $res['Id']; ?>">Elimina<?php }}?></a></td>
+						</tr>
+					</table>
+				</div>
 			</div>
-					</div>
-				
-			</div><!-- tab-content clearfix -->
 		</div>
 	</body>
 </html>
