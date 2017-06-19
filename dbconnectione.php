4001df5134a151eea3a8d622a1cc649d1ed8574d
salvo1994tpe <salvo1994tpe@gmail.com>
2017-06-19 12:00:17 +0200

    SISTEMATO IL CODICE DELLA QUERY DELLA PAGINA admin_inserisci_voti_next.php\

--- dbconnectione.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$connessione = mysqli_connect("127.0.0.1", "root", "", "progetto_accademia");
-	
-	if($connessione){
-		//echo "Connessione al database riuscita!!!!!!!!!!!";
-	} else {
-		echo "<div style=\"width:100%;background-color:#FF3333;text-align:center;\"><b>Connessione al database non riuscita. Riprova ad eseguire il login</b></div>";
-	}
-?>
