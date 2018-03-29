1c3c8fdcce02b68988ec701e084ed0eae20b6376
DanieleSupinfo <cozzolino_daniele@virgilio.it>
2018-03-29 12:33:06 +0200

    sistemata pagina "ripristina_password.php"

--- ripristina_password.php
+++ ripristina_password.php
@@ -38,18 +38,18 @@ if (isset ($_SESSION['iscritto-aggiunto'])){
 						 <div class="row form-group">
 							<p><a href="index.php"><b>&lt;&lt; Torna alla pagina di Login</b></a></p>
 						 </div>
-						<section class="loginform cf" style="float:left;">
+						<section class="loginform cf" >
 						<form name="register" action="registrati_query.php" method="post" accept-charset="utf-8">
 						<div class="row form-group">
 							<h2>Ripristina password:</h2>
 						</div>
-						
 						<div class="row form-group">
+						<div class="col-md-4">
 							<label>Email:</label>
 							<input type="text" class="form-control" name ="nome" value = "inserisci la tua email" required>
 						</div> <!-- /row form-group (1) -->	
 						
-						<div class="row form-group">
+						<div class="col-md-4">
 							<label>Scegli domanda di recupero: &nbsp;</label>
 							<select class="form-control" >
 								 <option>Nome di un tuo parente</option>
@@ -58,30 +58,21 @@ if (isset ($_SESSION['iscritto-aggiunto'])){
 							</select>
 						</div> <!-- /row form-group (10) -->
 						
-						<div class="row form-group">
+						<div class="col-md-4">
 							<label>Risposta:</label>
 							<input type="text" class="form-control" name ="nome" value = "inserisci risposta qui" required>
 						</div> <!-- /row form-group (1) -->		
-						
-						<div class="row form-group">	
+						</div>
+						<div class="row form-group pull-right">
+						<div class="col-md-12">						
 							<input type="submit" class="btn btn-info" value="Avanti">
+							</div>
 						</div> <!-- /row form-group (12) -->
 				</form>		
 			</section>
 		</div>	
 	
-<!-- javascript ================================================== --><!-- Placed at the end of the document so the pages load faster -->
-<!-- <script src="js/jquery.js"></script>
-<script src="js/jquery.easing.1.3.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/jquery.fancybox.pack.js"></script>
-<script src="js/jquery.fancybox-media.js"></script>
-<script src="js/google-code-prettify/prettify.js"></script>
-<script src="js/portfolio/jquery.quicksand.js"></script>
-<script src="js/portfolio/setting.js"></script>
-<script src="js/jquery.flexslider.js"></script>
-<script src="js/animate.js"></script>
-<script src="js/custom.js"></script> -->
+
 <?php @include_once 'shared/footer.php'; ?>
 </body>
 </html>
