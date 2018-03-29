5d4aa867aeb07174634d55286c01c782010d85a1
DanieleSupinfo <cozzolino_daniele@virgilio.it>
2018-03-29 17:35:52 +0200

    Sistemato il copyright del footer

--- shared/footer.php
+++ shared/footer.php
@@ -3,7 +3,7 @@
 <!-- INIZIO FOOTER -->
 <footer class="navbar-default navbar-inverse navbar-fixed-bottom" >
     <p align="center">
-        Copyright © 2015 Accademia Di Belle Arti Kandinskij
+        Copyright © 2018 Accademia Di Belle Arti Kandinskij
         <a href="" rel="nofollow" target="_blank"></a>
     </p>
 </footer>
