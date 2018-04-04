0e13f396bd42a3d831163bc25dafecfe0744a62a
DanieleSupinfo <cozzolino_daniele@virgilio.it>
2018-04-04 12:16:35 +0200

    aggiustato bug

--- admin_elimina_docente_query.php
+++ admin_elimina_docente_query.php
@@ -1,12 +1,16 @@
 <?php
 @include_once 'dbconnection.php';
 
-$id_docente=$_GET['ID'];
-echo "Id docente: ".$id_docente; 
+$id_anagrafe=$_GET['ID'];
+echo "Id anagrafe: ".$id_anagrafe; 
 
-$sql_delete_docente="DELETE FROM anagrafe WHERE Id='$id_docente'";
+$sql_delete_anagrafe="DELETE FROM anagrafe WHERE Id='$id_anagrafe'";
 echo "<br>Query: ".$sql_delete_docente;
 
+$sql_delete_docente="DELETE FROM docenti WHERE Id_anagrafe='$id_anagrafe'";
+echo "<br>Query: ".$sql_delete_docente;
+
+$connessione->query($sql_delete_anagrafe);
 $connessione->query($sql_delete_docente);
 
 @header('location:admin_visualizza_docenti.php');
