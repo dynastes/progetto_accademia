115a2d2c6a63bdb5c62aaed00cf78e0b52a60cf5
salvo1994tpe <salvo1994tpe@gmail.com>
2018-05-19 13:09:50 +0200

    Iniziato layout dashboard

--- shared/head_inclusions.php
+++ shared/head_inclusions.php
@@ -20,6 +20,7 @@
 <script src="js/bootstrap.min.js"></script>
 <script src="js/select/js/bootstrap-select.js"></script>
 
-
+<!-- fontawesome -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
 <!-- da commentare in futuro-->
 <!--meta name="viewport" content="width=device-width, initial-scale=1.0" /-->
