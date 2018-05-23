77e67c07dbe02f514fc7ad5c8a4c41a1666faa63
salvo1994tpe <salvo1994tpe@gmail.com>
2018-05-23 11:23:34 +0200

    modifiche alla pagina admin_dashboard.php nuovo css per dashboard "dashboard.css"

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -2,30 +2,13 @@
 //@session_start();
 @include_once 'dbconnection.php';
 
-/*prendere dalla tabella la lista di settori con questi campi:
-	- ID settore
-	- codice
 
-*/
-
-$sql_carica_settore="SELECT * FROM settore";
-$res_settore=$connessione->query($sql_carica_settore);
-//$res_settore_lista=$res_settore->fetch_assoc();
-
-/*
-
-Prendere inoltre i seguenti campi dalla tabella "materie_anagrafica":
-	- Id
-	- Nome_materia
-	- Id_settore (qui dentro, invece, salvare il valore del settore selezionato nel form)
-*/
-/*<!DOCTYPE html>
-<meta charset="UTF-8">	*/
 ?>
 
 <html>
 	<head>
 		<?php @include_once 'shared/head_inclusions.php';?>
+		<link href="css/dashboard.css" rel="stylesheet">
 		<?php @include_once 'shared/menu.php';?>
 			<?php
 			if($utente->get_ruolo() !="admin" and $utente->get_ruolo() != "editor"){
@@ -48,37 +31,37 @@ Prendere inoltre i seguenti campi dalla tabella "materie_anagrafica":
 			</div>
 			<div class="row">
 				<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-					<button class="btn btn-default custom-button"> <i class="fas fa-user"> </i> &nbsp Profilo </button>
+					<a href="profilo.php" class="btn btn-default custom-button"> <i class="fas fa-user"> </i> &nbsp Profilo </a>
 				</div>
 				<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-					<button class="btn btn-default custom-button"><i class="fas fa-check-square"> </i> &nbsp Feedback </button>
+					<a href="" class="btn btn-default custom-button"><i class="fas fa-check-square"> </i> &nbsp Feedback </a>
 				</div>
 				<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-					<button class="btn btn-default custom-button"><i class="fas fa-calendar-alt"> </i> &nbsp Calendario </button>
-				</div>
-			</div>
-			<br>
-			<div class="row">
-				<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-					<button class="btn btn-default custom-button"><i class="fas fa-book"> </i> &nbsp Materie </button>
-				</div>
-				<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-					<button class="btn btn-default custom-button"><i class="fas fa-file-alt"> </i> &nbsp Documenti </button>
-				</div>
-				<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-					<button class="btn btn-default custom-button"><i class="fas fa-chalkboard-teacher"> </i> &nbsp Docenti </button>
+					<a href="admin_imposta_orari_lezione.php" class="btn btn-default custom-button"><i class="fas fa-calendar-alt"> </i> &nbsp Calendario </a>
 				</div>
 			</div>
 			<br>
 			<div class="row">
 				<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-					<button class="btn btn-default custom-button"><i class="fas fa-info"> </i> &nbsp Richieste </button>
+					<a class="btn btn-default custom-button"><i class="fas fa-book"> </i> &nbsp Materie </a>
 				</div>
 				<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-					<button class="btn btn-default custom-button"><i class="fas fa-user-graduate"> </i> &nbsp Allievi </button>
+					<a href="admin_gestione_finanze.php" class="btn btn-default custom-button"><i class="fas fa-file-alt"> </i> &nbsp Documenti </a>
 				</div>
 				<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-					<button class="btn btn-default custom-button"><i class="fas fa-user-edit"> </i> &nbsp  Editors</button>
+					<a class="btn btn-default custom-button"><i class="fas fa-chalkboard-teacher"> </i> &nbsp Docenti </a>
+				</div>
+			</div>
+			<br>
+			<div class="row">
+				<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+					<a href="admin_gestisci_certificati.php" class="btn btn-default custom-button"><i class="fas fa-info"> </i> &nbsp Richieste </a>
+				</div>
+				<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+					<a class="btn btn-default custom-button"><i class="fas fa-user-graduate"> </i> &nbsp Allievi </a>
+				</div>
+				<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+					<a class="btn btn-default custom-button"><i class="fas fa-user-edit"> </i> &nbsp  Editors</a>
 				</div>
 			</div>
 			<br>
@@ -87,7 +70,7 @@ Prendere inoltre i seguenti campi dalla tabella "materie_anagrafica":
 				
 				</div>
 				<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-					<button class="btn btn-danger custom-button"><i class="fas fa-door-open"> </i> &nbsp Disconnetti </button>
+					<a class="btn btn-danger custom-button"><i class="fas fa-door-open"> </i> &nbsp Disconnetti </a>
 				</div>
 			</div>
 		</div>
